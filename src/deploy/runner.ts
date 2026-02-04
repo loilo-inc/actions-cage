a8a37120d17fd980c884bd5d8549a993efd41320
Yusuke Sakurai <kerokerokerop@gmail.com>
2026-02-04 16:17:30 +0900

    Centralize runner error handling (#12)

--- src/deploy/runner.ts
+++ src/deploy/runner.ts
@@ -29,32 +29,25 @@ export async function run() {
   const token = core.getInput("github-token");
   const ref = core.getInput("github-ref");
   const repository = core.getInput("github-repository");
-  try {
-    let deployment: GithubDeploymentParams | undefined;
-    if (createDeployment) {
-      deployment = aggregateDeploymentParams({
-        ref,
-        repository,
-        environment,
-        token,
-      });
-    }
-    const args = ["--region", region];
-    if (idleDuration) {
-      args.push("--canaryTaskIdleDuration", idleDuration);
-    }
-    if (updateService) {
-      args.push("--updateService");
-    }
-    if (cageOptions) {
-      args.push(...parseStringToArgs(cageOptions));
-    }
-    args.push(deployContext);
-    await deploy({ deployment, args });
-  } catch (e) {
-    if (e instanceof Error) {
-      core.error(e);
-    }
-    core.setFailed("see error above");
+  let deployment: GithubDeploymentParams | undefined;
+  if (createDeployment) {
+    deployment = aggregateDeploymentParams({
+      ref,
+      repository,
+      environment,
+      token,
+    });
   }
+  const args = ["--region", region];
+  if (idleDuration) {
+    args.push("--canaryTaskIdleDuration", idleDuration);
+  }
+  if (updateService) {
+    args.push("--updateService");
+  }
+  if (cageOptions) {
+    args.push(...parseStringToArgs(cageOptions));
+  }
+  args.push(deployContext);
+  await deploy({ deployment, args });
 }
