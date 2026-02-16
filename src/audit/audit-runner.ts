c1fc38ebd88042038c19ce07a3be5c86ca3ef397
Yusuke Sakurai <kerokerokerop@gmail.com>
2026-02-16 16:55:40 +0900

    fix(audit): Added default issue title to action.yml

--- src/audit/audit-runner.ts
+++ src/audit/audit-runner.ts
@@ -8,16 +8,19 @@ export async function run() {
   const auditContexts = core.getInput("audit-contexts");
   const auditServices = core.getInput("audit-services");
   const cageOptions = core.getInput("cage-options");
-  const issueTitle = core.getInput("issue-title") || `Cage audit report`;
   const dryRun = boolify(core.getInput("dry-run"));
-  const { owner, repo } = (() => {
+  const { owner, repo, issueTitle } = (() => {
+    const issueTitle = core.getInput("issue-title");
     const repository = process.env.GITHUB_REPOSITORY;
     const m = repository?.match(/^(.+?)\/(.+?)$/);
     if (!m) {
       throw new Error(`GITHUB_REPOSITORY is not set or invalid: ${repository}`);
     }
+    if (issueTitle.trim() === "") {
+      throw new Error("issue-title input cannot be empty");
+    }
     const [owner, repo] = [m[1], m[2]];
-    return { owner, repo };
+    return { owner, repo, issueTitle };
   })();
   const fullArgsList = Array.from(
     iterateAuditTargets({
